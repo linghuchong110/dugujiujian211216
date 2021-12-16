300eb43bb6030efdeed30f2c9bb709ef63432085
yuebuqun <1304083645@qq.com>
2021-12-16 12:30:16 +0800

    岳不群修改

--- src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
@@ -2,6 +2,10 @@ package com.atguigu.jianfa.dugujiujian;
 
 public class Dugu {
     public static void main(String[] args) {
-        System.out.println("jianfa dugujiujian!!!!!!!!!!!!"); 
+        System.out.println("jianfa dugujiujian!!!!!!!!!!!!");
+        System.out.println("jianfa dugujiujian!!!!!!!!!!!!");
+        System.out.println("jianfa dugujiujian!!!!!!!!!!!!");
+        System.out.println("jianfa dugujiujian!!!!!!!!!!!!");
+
     }
 }
