c5693619a01575e270da251e1691ea11b97febca
linghuchong110 <1245920053@qq.com>
2021-12-16 17:12:47 +0800

    newjianfa

---
+++ src/main/java/newjianfa.java
@@ -0,0 +1,9 @@
+public class newjianfa {
+    public static void main(String[] args) {
+        System.out.println("jianfa！！1");
+        System.out.println("jianfa！！1");
+        System.out.println("jianfa！！1");
+        System.out.println("jianfa！！1");
+
+    }
+}
